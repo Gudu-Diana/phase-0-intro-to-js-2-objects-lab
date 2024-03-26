dae84004071da160b3fd20b441a4fb13b4e391ce
diana <gududiana@gmail.com>
2024-03-26 11:31:38 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// Initialize employee object
+  let employee = {
+    name: 'diana',
+    streetAddress: 'plateau road'
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let clone = { ...employee };
+    delete clone[key];
+    return clone;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
